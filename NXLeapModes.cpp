1597bb3af10f65713cab3426421d0ee144b361de
Kyle Rector <kylearector@outlook.com>
2015-06-03 07:17:56 -0400

    Update NXLeapModes.cpp

--- NXLeapModes.cpp
+++ NXLeapModes.cpp
@@ -166,18 +166,18 @@ void WindListener::onFrame(const Controller& controller) {
 }// End object
 
 int main() {
-  // Create a sample listener and controller
+  // Create a listener and controller
   WindListener listener;
   Controller controller;
 
-  // Have the sample listener receive events from the controller
+  // Have the listener receive events from the controller
   controller.addListener(listener);
 
   // Keep this process running until Enter is pressed
   std::cout << "Press Enter to quit..." << std::endl;
   std::cin.get();
 
-  // Remove the sample listener when done
+  // Remove the listener when done
   controller.removeListener(listener);
 
   return 0;
